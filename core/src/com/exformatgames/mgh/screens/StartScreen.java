8534bd2ca3757f70a4cd0e0c0cc6965a6b5a8b4f
exformat <exformat1988@gmail.com>
2022-07-15 21:07:47 +0600

    init

--- core/src/com/exformatgames/mgh/screens/StartScreen.java
+++ core/src/com/exformatgames/mgh/screens/StartScreen.java
@@ -2,18 +2,24 @@ package com.exformatgames.mgh.screens;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 
 public class StartScreen implements Screen {
 
     private final Game game;
+    private final AssetManager assetManager;
+    private final TextureAtlas atlas;
 
-    public StartScreen(Game game) {
+    public StartScreen(Game game, AssetManager assetManager, TextureAtlas atlas) {
         this.game = game;
+        this.assetManager = assetManager;
+        this.atlas = atlas;
     }
 
     @Override
     public void show() {
-
+        game.setScreen(new GameScreen(game, assetManager, atlas));
     }
 
     @Override
